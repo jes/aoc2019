2a407c51af1eb82fdbefd72babb5e0c525f8fae4
James Stanley <james@incoherency.co.uk>
2019-12-15 05:36:36 +0000

    15
    Hilariously, I accidentally wrote "pop" instead of "shift" in the shortest path
    function. Worked anyway. Lol.

--- 15b.pl
+++ 15b.pl
@@ -11,6 +11,8 @@ my $pid = open3(\*C_IN, \*C_OUT, \*C_ERR, "./run-program-15 15.in")
 my %map;
 my ($x, $y) = (0, 0);
 
+my ($oxx, $oxy) = (0,0);
+
 $map{"0,0"} = 'D';
 
 my @xdir = (0, 0, 1, -1);
@@ -23,9 +25,11 @@ shortestpath();
 
 sub shortestpath {
     my %visited;
-    my @q = ([0,0,0]);
+    my @q = ([$oxx,$oxy,0]);
 
-    $visited{"0,0"} = 1;
+    my $longest = 0;
+
+    $visited{"$oxx,$oxy"} = 1;
     while (@q) {
         my $node = pop @q;
         my ($x, $y, $len) = @$node;
@@ -34,12 +38,14 @@ sub shortestpath {
             my $newx = $x + $xdir[$d];
             my $newy = $y + $ydir[$d];
             next if $map{"$newx,$newy"} eq '#';
-            print "" . ($len+1) . "\n" if $map{"$newx,$newy"} eq 'O';
             next if $visited{"$newx,$newy"};
             $visited{"$newx,$newy"} = 1;
             push @q, [$newx,$newy,$len+1];
+            $longest = $len+1 if $len+1 > $longest;
         }
     }
+
+    print "$longest\n";
 }
 
 sub dfs {
@@ -54,6 +60,7 @@ sub dfs {
             $map{"$newx,$newy"} = '#';
         } else {
             $map{"$newx,$newy"} = ($r == 1 ? '.' : 'O');
+            ($oxx,$oxy)=($newx,$newy) if $r==2;
             ($x,$y) = ($newx,$newy);
             dfs();
             $x -= $xdir[$d];
